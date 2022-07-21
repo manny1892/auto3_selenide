7c39edc7d50e345cf87d3db64741e63eea3c8c4d
manny1892 <manny1892@gmail.com>
2022-07-21 21:26:36 +0300

    homework CI tests dont work

--- src/test/java/BankServiceTest.java
+++ src/test/java/BankServiceTest.java
@@ -8,14 +8,24 @@ import static com.codeborne.selenide.Selenide.open;
 
 class BankServiceTest {
     @Test
-    void test() {
+    void shouldOrderCard() {
         open("http://localhost:9999/");
         SelenideElement form = $(".form");
         form.$("[data-test-id='name'] input").setValue("Иванов Иван");
         form.$("[data-test-id='phone'] input").setValue("+79099876545");
         form.$("[data-test-id='agreement']").click();
         form.$("button").click();
-        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее врем."));
+    }
+
+    @Test
+    void shouldValidField() {
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='phone'] input").setValue("+79099876545");
+        form.$("[data-test-id='agreement']").click();
+        form.$(".button").click();
+        $(".input__sub").shouldHave(exactText("Поле обязательно для заполнени"));
     }
 
 }
