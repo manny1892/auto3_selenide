19780e09889fe78431dc5441e629005921d7af93
manny1892 <manny1892@gmail.com>
2022-07-21 20:07:10 +0300

    homework #1 commit

--- src/test/java/BankServiceTest.java
+++ src/test/java/BankServiceTest.java
@@ -1,2 +1,41 @@
-public class BankServiceTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+class BankServiceTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Test
+    void test() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79099876545");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+
 }
