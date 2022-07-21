d42c5e6c3347c0c74bafe5821025d813ccaa3d74
manny1892 <manny1892@gmail.com>
2022-07-21 20:33:32 +0300

    homework #1 commit-fix

--- src/test/java/BankServiceTest.java
+++ src/test/java/BankServiceTest.java
@@ -1,41 +1,21 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 
 class BankServiceTest {
-
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
     @Test
     void test() {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Иванов Иван");
-        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79099876545");
-        driver.findElement(By.className("checkbox")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
-        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] input").setValue("Иванов Иван");
+        form.$("[data-test-id='phone'] input").setValue("+79099876545");
+        form.$("[data-test-id='agreement']").click();
+        form.$("button").click();
+        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
-
-    @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
-
 
 }
